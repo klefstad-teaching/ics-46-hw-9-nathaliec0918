c4f583151bb4c33417efd8d438e05d28775365b0
Nathalie Chiu <chiuht@uci.edu>
2025-03-10 04:33:17 +0000

    include cctype library

--- src/ladder.h
+++ src/ladder.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <cctype>
 
 using namespace std;
 
@@ -16,3 +17,4 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
 void load_words(set<string> & word_list, const string& file_name);
 void print_word_ladder(const vector<string>& ladder);
 void verify_word_ladder();
+// vector<string> get_words_from file(const string& file_name)
