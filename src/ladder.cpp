5315ad003439d547211af2656f3cbe956be663a6
Nathalie Chiu <chiuht@uci.edu>
2025-03-10 06:23:21 +0000

    wrote is_adjacent and edit_distance_within

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -11,15 +11,28 @@
 using namespace std;
 
 void error(string word1, string word2, string msg) {
-    cout << "with error for: " << word1 << " and " << word2 << endl;
+    cout << "error for (" << word1 << ", " << word2 << "): " << msg << endl;
 }
 
+// ensure str1 and str2 only have one char different
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
-    
+    int num_char_diff = 0;
+    if (str1.length() != str2.length() ) {
+        error(str1, str2, "length of two words are different.");
+        return false;
+    }
+    for (size_t i = 0; i < str1.length(); ++i) {
+        if (str1[i] != str2[i]) num_char_diff++;
+        if (num_char_diff > d) return false;
+    }
+    return true; // num_char_diff <= d
 }
-bool is_adjacent(const string& word1, const string& word2) {
 
+// call edit_distance_within and make sure it's true
+bool is_adjacent(const string& word1, const string& word2) {
+    return edit_distance_within(word1, word2, 1);
 }
+
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
 
 }
@@ -28,7 +41,7 @@ void load_words(set<string> & word_list, const string& file_name) {
     ifstream in(file_name);
     string word;
     while (in >> word) {
-        transform(word.begin(), word.end(), word.begin, ::tolower);
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
         word_list.insert(word);
     }
     in.close();
