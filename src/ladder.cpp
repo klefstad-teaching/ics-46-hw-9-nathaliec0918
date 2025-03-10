e9c8109c0cef8335a7a8ecf1bbaf54ee956076db
Nathalie Chiu <chiuht@uci.edu>
2025-03-10 06:28:39 +0000

    words of different lengths are allowed

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -17,10 +17,10 @@ void error(string word1, string word2, string msg) {
 // ensure str1 and str2 only have one char different
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
     int num_char_diff = 0;
-    if (str1.length() != str2.length() ) {
-        error(str1, str2, "length of two words are different.");
-        return false;
-    }
+    // if (str1.length() != str2.length() ) {
+    //     error(str1, str2, "length of two words are different.");
+    //     return false;
+    // }
     for (size_t i = 0; i < str1.length(); ++i) {
         if (str1[i] != str2[i]) num_char_diff++;
         if (num_char_diff > d) return false;
@@ -48,8 +48,11 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
-
+    for (string word : ladder) {
+        cout << word << " ";
+    }
 }
+
 void verify_word_ladder() {
 
 }
