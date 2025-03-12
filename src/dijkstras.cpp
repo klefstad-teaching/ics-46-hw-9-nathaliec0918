e812d22f543bbcad141d9de380c3abbd034e0959
Nathalie Chiu <chiuht@uci.edu>
2025-03-12 05:41:03 +0000

    wrote dijkstras.cpp and main

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -8,40 +8,61 @@
 
 using namespace std;
 
-// vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
-//     int num = G.numVertices;
-//     vector<int> distances(num, INF);
-//     vector<bool> visited(num, false);
-//     distances[source] = 0;
-//     previous[source] = -1;
-//     // initialize as minHeap
-//     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap; 
-//     minHeap.push({source, 0});
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int num = G.numVertices;
+    vector<int> distances(num, INF);
+    vector<bool> visited(num, false);
+    distances[source] = 0;
+    previous[source] = -1;
+    // initialize as minHeap
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap; 
+    minHeap.push({source, 0});
 
-//     while (!minHeap.empty()) {
-//         int src = minHeap.top().first;
-//         minHeap.pop();
-//         if (visited[src]) continue;
-//         visited[src] = true;
-//         for (const Edge & edge : graph[src]) {
-//             int this_src = edge.src;
-//             int dst = edge.dst;
-//             int weight = edge.weight;
-//             if (!visited[this_src] && distances[src] + weight < distances[this_src]) {
-//                 distances[this_src] = distances[src] + weight;
-//                 previous[this_src] = src;
-//                 minHeap.push({this_src, distances[this_src]});
-//             }
-//         }
-//     }
-//     return distances;
-// }
+    while (!minHeap.empty()) {
+        int src = minHeap.top().first;
+        minHeap.pop();
+        if (visited[src]) continue;
+        visited[src] = true;
+        for (const Edge & edge : G[src]) {
+            // int this_src = edge.src;
+            int dst = edge.dst;
+            int weight = edge.weight;
+            if (!visited[dst] && distances[src] + weight < distances[dst]) {
+                distances[dst] = distances[src] + weight;
+                previous[dst] = src;
+                minHeap.push({dst, distances[dst]});
+            }
+        }
+    }
+    return distances;
+}
 
-// vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
-    
-// }
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) { //go backwardsd from previous from destination
+    vector<int> path;
+    for (int i = destination; i != -1; i = previous[i]) {
+        path.push_back(i);
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
 
-// void print_path(const vector<int>& v, int total) {
+void print_path(const vector<int>& v, int total) {
+    for (const int & each : v) {
+        cout << each << " ";
+    }
+    cout << endl;
+    cout << "Total cost is " << total << endl;
+}
 
-//     cout << "Total cost is " << total << endl;
-// }
+string get_arg(int argc, char *argv[], string def) {
+    if (argc > 1)
+        return argv[1];
+    else
+        return def;
+}
+
+void file_to_graph(string filename, Graph & G) {
+    ifstream in(filename);
+    in >> G;
+    in.close(); 
+}
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,1 +1,18 @@
 #include "dijkstras.h"
+
+int main(int argc, char *argv[]) {
+    string file_name = "small.txt";
+    string file_name_src = "src/"+file_name;
+    cout << "dijkstras " << file_name << endl;
+    string filename = get_arg(argc, argv, file_name_src);
+    Graph G;
+    file_to_graph(filename, G);
+
+    vector<int> previous(G.numVertices);
+    vector<int> distances = dijkstra_shortest_path(G, 0, previous);
+    for (int vertex = 0; vertex < G.numVertices; ++vertex) {
+        vector<int> path = extract_shortest_path(distances, previous, vertex);
+        print_path(path, distances[vertex]);
+    }
+    return 0;
+} 
